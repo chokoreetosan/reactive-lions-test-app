4e94f278a8b735d5130aa5211161b72a7780037e
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2021-04-17 14:17:46 -0700

    put postercontainer inside contentcontainer, along with sidenavbar

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import PosterContainer from './PosterContainer';
 import SideNavBar from './SideNavBar'
 
 const ContentContainer = () => {
 
     return <div>
         <SideNavBar/>
+        <PosterContainer />
         </div>
 }
 
