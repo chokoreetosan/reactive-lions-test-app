7ab510789f8395fc866fc852f249744f2fa03ed6
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2021-04-17 14:25:04 -0700

    added file for Movie Data upon poster Click

---
+++ src/components/MovieData.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Data = styled.div`
+
+`;
+
+const MovieData = () => {
+    return <Data>
+        
+    </Data>
+}
+
+export default MovieData;
