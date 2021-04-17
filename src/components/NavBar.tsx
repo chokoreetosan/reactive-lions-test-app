4791a4d006d512e00f27be43815048743ade3b70
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2021-04-17 14:09:53 -0700

    added icon, centered link to default Discover Tab

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,15 +2,17 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
+import icon from '../images/gatsby-icon.png'
 const Bar = styled.div`
 margin:0 auto;
 max-width:960px;
-height:
+height: 10%;
 padding:1rem 1.0875rem;
 display:flex;
 flex-direction:row;
 align-items:center;
-justify-content:center;
+justify-content:start;
+border-bottom: 1px solid black;
 `;
 
 interface NavBarProps {
@@ -18,20 +20,35 @@ interface NavBarProps {
 
 const NavBar = ({ }:NavBarProps) => (
     <Bar id={'navbar'}>
-
-        <Link
-          to="/"
+        <img src={icon} style={{height:'3em', width:'auto'}}/>
+        <div
+        style={{
+          height:'100%',
+          width:'100%',
+          display:'flex',
+          flexDirection:'row',
+          justifyContent:"center",
+          alignItems:"center"
+        }}
+        >
+        <button
+          onSubmit={()=>{
+            console.log('navigates to Discover Tab')
+          }}
           style={{
             color: `black`,
+            backgroundColor:"rgba(0,0,0,0)",
             fontSize:'1.5em',
             fontWeight:'bold',
             fontFamily:'Arial, Helvetica, sans-serif',
             textDecoration: `none`,
+            border:'none'
           }}
         >
           Discover
-        </Link>
-    </Bar>
+        </button> 
+        </div>
+      </Bar>
 )
 
 
