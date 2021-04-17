4095069d5a475550bbaedd116e691b9ae726b60a
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2021-04-17 16:40:12 -0700

    poster now renders with default picture from gatsby-image

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,2 +1,32 @@
 import * as React from 'react';
 import styled from 'styled-components'
+
+interface PosterProps {
+    movieData: {
+        Director:string,
+        Genres:string,
+        IMDB_Rating:string,
+        Lead_Actors:string,
+        Poster:string,
+        Summary:string,
+        Title:string,
+        Year:string,
+        id:string
+    }
+    image:string
+}
+
+const Image = styled.img`
+width:15vh;
+height:auto;
+border-radius:5px;
+border:3px solid #A742EB;
+`;
+
+const Poster = ({movieData, image}:PosterProps) => {
+    console.log(movieData, image)
+    return <Image src={image} />
+    
+}
+
+export default Poster
