425e77e6b64dae2d5c8f0f725b69cd6e768a9821
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2021-04-18 15:37:25 -0700

    moved movieData type to it's own file

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+interface movieData {
+    Director: string
+    Genres: string
+    IMDB_Rating: string
+    Lead_Actors: string
+    Poster: string
+    Summary: string
+    Title: string
+    Year: string
+    id: string
+    imageURL: string
+  }
